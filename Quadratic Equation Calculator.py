9c097ea32ad3bebf958106e13dad75f2a7d525bc
Nicholas Ong <68470261+n1cholasong@users.noreply.github.com>
2020-07-18 22:19:08 +0800

    Add files via upload

---
+++ Quadratic Equation Calculator.py
@@ -0,0 +1,33 @@
+import math
+print('Quadratic Equation Calculator')
+
+for x in range(999):
+    
+    print('')
+    
+    a = float(input('A = '))
+    b = float(input('B = '))
+    c = float(input('C = '))
+
+    d = (b**2) - (4*a*c)
+
+    print('')
+    print('Discriminant =', d) 
+    
+    if d < 0:
+        print('No real solution!')
+
+    elif d > 0:
+        sqrt = math.sqrt(d)
+        sol1 = (-b + math.sqrt(d)) / (2*a)
+        sol2 = (-b - math.sqrt(d)) / (2*a)
+        print('=', sqrt)
+        print('\nx = %s\nx = %s' %(sol1, sol2))
+
+    
+
+    print('')
+    print('=============== NEXT QUESTION ===============')
+    
+   
+
