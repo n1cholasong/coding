b359a70e65ef360fccc11848288ecf55ce9f4eaa
Nicholas Ong <68470261+n1cholasong@users.noreply.github.com>
2020-07-18 22:44:53 +0800

    Update Cyber Challenge.py

--- Cyber Challenge.py
+++ Cyber Challenge.py
@@ -1,4 +1,3 @@
-
 from random import randint
 
 p = True
@@ -7,16 +6,16 @@ b = 0   #multiplyer for variable y
 x = 0   #the ASCCI code  
 y = 0   #the ASCCI code 
 
-while p == True:            #program will continues to run forever until p == false
-    x = randint(65,90)      #generates ASCII code of A-Z
-    y = randint(97, 122)    #generates ASCII code of a-z
+while p == True:                                      #program will continues to run forever until p == false
+    x = randint(65,90)                                #generates ASCII code of A-Z
+    y = randint(97, 122)                              #generates ASCII code of a-z
 
-    a = randint(0, 9)       #generate the limits of multiplication for variable a
-    b = randint(0, 9)       #generate the limits of multiplication for variable b
+    a = randint(0, 9)                                 #generate the limits of multiplication for variable a
+    b = randint(0, 9)                                 #generate the limits of multiplication for variable b
+             
+    c1 = a + b                                        #equation for condition 1, as only 9 character is needed
 
-    c1 = a + b              #equation for condition 1, as only 9 character is needed
-
-    if c1 == 9:             #when condition is met, the following output will be process
-        c2 = (a*x) + (b*y)  #equation for condition 2
-        if c2 == 896:       #when condition is met, the following output will be process
-            print('%s(%s) + %s(%s)'%(a ,x, b, y)) #this will print the possibilities
+    if c1 == 9:                                       #when condition is met, the following output will be process
+        c2 = (a*x) + (b*y)                            #equation for condition 2
+        if c2 == 896:                                 #when condition is met, the following output will be process
+            print('%s(%s) + %s(%s)'%(a ,x, b, y))     #this will print the possibilities
